dab8c120fe8cffb4c2959288ddfa6d8b29c34ae9
Arthur Aguiar <sepulven40028922@sepulven.com>
2022-10-17 23:39:22 +0100

    backing up

--- statics/scripts/script.js
+++ statics/scripts/script.js
@@ -1,11 +1,62 @@
-const icon = document.getElementById('icon')
-const menu = document.getElementById('menu')
-icon.addEventListener('click', () => {
-    let attr = 'invisible'
-    if (menu.classList.contains(attr)) {
-    menu.classList.remove(attr)
-    } else {
-        menu.classList.add(attr)
-    }
-    console.log(menu.classList)
-})
+    const i_articles = document.getElementById('icon-articles')
+    const i_hamburguer = document.getElementById('icon-hamburguer')
+    const hamburguer_menu = document.getElementById('hamburguer_menu')
+    const articles_menu = document.getElementById('articles_menu')
+
+// TODO: Refactorar o código, estamos a nos repetir
+
+    let attr = 'hidden'
+//! Hardcode 
+if (window.innerWidth < 640) {
+    hamburguer_menu.classList.add(attr)
+}
+
+    window.addEventListener('resize', e => {
+        if (window.innerWidth > 640) {
+            hamburguer_menu.classList.remove(attr)
+        } else {
+            hamburguer_menu.classList.add(attr)
+        }
+    })
+    i_hamburguer.addEventListener('click', e => {
+        if (hamburguer_menu.classList.contains(attr)) {
+            i_hamburguer.src = 'imgs/close.svg'
+            hamburguer_menu.classList.remove(attr)
+        } else {
+            i_hamburguer.src = 'imgs/hamburguer.svg'
+            hamburguer_menu.classList.add(attr)
+        }
+    })
+    i_articles.addEventListener('click', () => {
+        console.log(window.screen.width > 64)
+        if (articles_menu.classList.contains(attr)) {
+            articles_menu.classList.remove(attr)
+        } else {
+            articles_menu.classList.add(attr)
+        }
+    })
+
+const articles = document.querySelectorAll('article')
+
+articles.forEach(article => {
+    let leftColor = 'bg-grey'
+    let rightColor = 'bg-white'
+    let left = article.firstChild.classList
+    let right = article.lastChild.classList
+
+    article.addEventListener('mouseover', e => {
+        if(left.contains(leftColor) ||
+           right.contains(rightColor)) {
+            console.log('dentro')
+            left.remove(leftColor)
+            right.remove(rightColor)
+        } else {
+            left.add(leftColor)
+            right.add(rightColor)
+        }
+    })
+    article.addEventListener('mouseout', e => {
+        left.remove(leftColor)
+        right.remove(rightColor)
+    })
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,19 @@ module.exports = {
     colors: {
       'grey': '#2f2d2e',
       'light-grey': '#373f51',
+      'bluish-gray': '#242937',
       'black': '#000',
       'white': '#FEF9FF',
-      'rose': '#F3E8E3'
+      'rose': '#F3E8E3',
+      'red': '#FF0000'
     },
     fontFamily: {
       'title' : '"Saira Semi Condensed"',
       'subtitle' : 'Montserrat ',
       'body' : '"Red Hat Display"'
+    },
+    backgroundImage: {
+      'sea-sky': "url('../imgs/sky-sea.jpg')" // Estamos dentro da pagina styles
     }
   },
   plugins: [],
